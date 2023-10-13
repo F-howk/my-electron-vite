88f77e57c339c991a01e8332402478e4aa57fd87
zc <1135010828@qq.com>
2023-10-13 18:00:46 +0800

    hash模式

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '@renderer/views/Home/IndexPage.vue'
 
@@ -11,7 +11,7 @@ const home = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [...home]
 })
 
